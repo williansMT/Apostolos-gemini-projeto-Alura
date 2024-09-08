6de885db3e5530348f50ad760e9a3364ffa4274b
williansMT <65174371+williansMT@users.noreply.github.com>
2024-09-07 22:09:12 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,110 +1,226 @@
-
 let dados = [
+
   {
-    nome: "simão pedro" ,
-    periodo: "Século I",
-    localNascimento: "Betsaida, Galileia",
-    localMartirio: "Roma",
-    martirio: "Crucificado de cabeça para baixo",
-    perseguidor: "Nero",
-    outrasInformacoes: "Líder dos apóstolos, recebeu as chaves do reino dos céus. Autor de algumas das cartas do Novo Testamento.",
-    link: "https://pt.wikipedia.org/wiki/Pedro_(ap%C3%B3stolo)",
     
+      nome: "Pedro",
+      apelidos: ["Cefas", "rocha"],
+      periodo: "c. 4-64 d.C.",
+      localNascimento: "Betsaida, Galileia",
+      localMartirio: "Roma",
+      martirio: "Crucificado de cabeça para baixo",
+      perseguidor: "Nero",
+      outrasInformacoes: "Líder dos apóstolos, recebeu as chaves do reino dos céus. Autor de duas cartas e de alguns dos discursos nos Atos dos Apóstolos.",
+      link: "https://pt.wikipedia.org/wiki/Sim%C3%A3o_Pedro",
+      discipuloDe: "João Batista",
+      eventosImportantes: ["Confissão de Pedro", "Negação de Jesus", "Pentecostes"],
+      patronoDe: ["Pescadores", "Roma"],
+      festaLiturgica: "29 de junho",
+      iconografia: "Chaves, galo",
+      milagresAtribuidos: ["Andar sobre as águas", "Ressurreição de Tabita"],
+      escritos: ["Primeira Carta de Pedro", "Segunda Carta de Pedro"]
+    },
+    {
+      nome: "Andre",
+      apelidos: ["O Protoklētos, significa o primeiro chamado em grego, referindo-se a ele ter sido um dos primeiros discípulos de Jesus."],
+      periodo: "c. 5-70 d.C.",
+      localNascimento: "Betsaida, Galileia",
+      localMartirio: "Patras, Grécia",
+      martirio: "Crucificado em forma de X",
+      perseguidor: "Égeo, procônsul da Acaia",
+      outrasInformacoes: "Um dos primeiros discípulos de Jesus, irmão de Pedro. Conhecido por sua pregação na Ásia Menor e Escítia.",
+      link: "https://pt.wikipedia.org/wiki/Andr%C3%A9,_o_Ap%C3%B3stolo",
+      discipuloDe: "João Batista",
+      eventosImportantes: ["Encontro com Jesus", "Apresentação de Pedro a Jesus", "Missão na Ásia Menor e Escítia"],
+      patronoDe: ["Rússia", "Escócia", "Marinha, Pescadores"],
+      festaLiturgica: "30 de novembro",
+      iconografia: "Cruz de Santo André"
+    },
+    {
+      nome: "Tiago, Filho de zebedeu",
+      apelidos: ["Tiago Maior", "Boanerges para diferenciá-lo de outro apóstolo chamado Tiago e Boanerges que significa filhos do trovão em aramaico, referindo-se a seu temperamento."],
+      periodo: "c. 4-44 d.C.",
+      localNascimento: "Betsaida, Galileia",
+      localMartirio: "Jerusalém",
+      martirio: "Decapitado por Herodes Antipas",
+      outrasInformacoes: "Um dos Doze Apóstolos, filho de Zebedeu e Salomé, irmão de João. Conhecido por seu temperamento forte e por ser um dos três discípulos mais próximos de Jesus.",
+      link: "https://pt.wikipedia.org/wiki/Tiago,_filho_de_Zebedeu",
+      discipuloDe: "João Batista",
+      eventosImportantes: ["Chamado por Jesus com João e Pedro", "Presença na Transfiguração", "Pedido de lugares de honra"],
+      patronoDe: ["Espanha", "Peregrinos"],
+      festaLiturgica: "25 de julho",
+      iconografia: "Concha de vieira, espada"
+    },
+  {
+    nome: "Joao",
+    apelidos: ["O Discípulo Amado"],
+    periodo: "c. 5-100 d.C.",
+    localNascimento: "Betsaida, Galileia",
+    localMartirio: "Èfeso,morte natural ",
+    localTrabalho: "Éfeso,onde acredita-se que tenha vivido e escrito seus livros.",
+    outrasInformacoes: "Um dos Doze Apóstolos, filho de Zebedeu e Salomé, irmão de Tiago. Conhecido por sua proximidade com Jesus e por ser autor de vários livros do Novo Testamento.",
+    link: "https://pt.wikipedia.org/wiki/Jo%C3%A3o,_o_Ap%C3%B3stolo",
+    discipuloDe: "João Batista",
+    eventosImportantes: ["Chamado por Jesus com Tiago e Pedro", "Presença na Última Ceia", "Testemunha da ressurreição"],
+    patronoDe: ["Teólogos, místicos"],
+    festaLiturgica: "27 de dezembro",
+    iconografia: "Águia, cálice, livro",
+    escritos: ["Evangelho de João", "Primeira Epístola de João", "Segunda Epístola de João", "Terceira Epístola de João", "Apocalipse"]   
+
   },
   {
-    nome: "andré",
-    periodo: "Século I",
+    nome: "Filipe",
+    periodo: "c. 1º século d.C.",
     localNascimento: "Betsaida, Galileia",
-    localMartirio: "Patras, Grécia",
+    localMartirio: "Hierápolis, Frígia, Tradição afirma que foi martirizado pendurado em cruzes em forma de X", // Tradição afirma que foi martirizado pendurado em cruzes em forma de X
     martirio: "Crucificado em forma de X",
-    outrasInformacoes: "Irmão de Pedro, evangelizou a região do Mar Negro.",
-    link: "https://pt.wikipedia.org/wiki/André_(apóstolo)"
+    outrasInformacoes: "Um dos Doze Apóstolos, conhecido por sua fé e por ter apresentado Natanael a Jesus. Foi um dos primeiros discípulos a seguir Jesus. A tradição afirma que pregou o Evangelho na Frígia e na Escítia.",
+    link: "https://pt.wikipedia.org/wiki/Filipe_(ap%C3%B3stolo)",
+    discipuloDe: "João Batista",
+    eventosImportantes: ["Chamado por Jesus", "Apresentação de Natanael a Jesus", "Missões missionárias na Frígia e Escítia"],
+    patronoDe: ["Frígia, Escítia"],
+    festaLiturgica: "14 de maio",
+    iconografia: "Cruz, livro",
   },
   {
-    nome: "tiago, filho de Zebedeu",
-    periodo: "Século I",
-    localNascimento: "Betsaida, Galileia",
-    localMartirio: "Jerusalém",
-    martirio: "Decapitado",
-    perseguidor: "Herodes Antipas",
-    outrasInformacoes: "Um dos apóstolos mais próximos de Jesus, junto com seu irmão João.",
-    link: "https://pt.wikipedia.org/wiki/Tiago_Maior"
+    nome: "Bartolomeu",
+    apelidos: ["Natanael"],
+    periodo: "c. 1º século d.C.",
+    localNascimento: "Canaã - Tradicionalmente associado à Galileia", // Tradicionalmente associado à Galileia
+    localMartirio: "Índia - Segundo algumas tradições, martirizado na Índia", // Segundo algumas tradições, martirizado na Índia
+    martirio: "Esfolado vivo - Tradicionalmente, martirizado por ter sua pele arrancada  ", // Tradicionalmente, martirizado por ter sua pele arrancada
+    outrasInformacoes: "Um dos Doze Apóstolos, conhecido por sua sinceridade e fé. Foi apresentado a Jesus por Filipe. A tradição afirma que evangelizou a Índia.",
+    link: "https://pt.wikipedia.org/wiki/Bartolomeu_(ap%C3%B3stolo)",
+    discipuloDe: "João Batista - Antes de seguir Jesus ", // Antes de seguir Jesus
+    eventosImportantes: ["Apresentação a Jesus por Filipe", "Missões missionárias na Índia"],
+    patronoDe: ["Tanners, curtidores, boticários - Devido à sua forma de martírio"], // Devido à sua forma de martírio
+    festaLiturgica: "24 de agosto",
+    iconografia: "Faca, livro - A faca simboliza seu martírio e o livro, sua pregação", // A faca simboliza seu martírio e o livro, sua pregação
+    milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+    escritos: ["Não há escritos canônicos atribuídos a Bartolomeu"] // Não há escritos canônicos atribuídos a Bartolomeu
   },
   {
-    nome: "joão",
-    periodo: "Século I",
-    localNascimento: "Betsaida, Galileia",
-    localMartirio: "Éfeso",
-    outrasInformacoes: "Autor do Evangelho de João, das três epístolas joaninas e do Apocalipse.",
-    link: "https://pt.wikipedia.org/wiki/João_evangelista"
+    nome: "Mateus",
+    apelidos: ["Leví"],
+    periodo: "c. 1º século d.C.",
+    localNascimento: "Cafarnaum,Cidade onde Jesus o chamou", // Cidade onde Jesus o chamou
+    profissao: "Publicano - Cobrador de impostos ", // Cobrador de impostos
+    outrasInformacoes: "Um dos Doze Apóstolos, anteriormente cobrador de impostos. Autor do Evangelho de Mateus, que enfatiza a realeza de Jesus e a continuidade entre a Lei e o Evangelho.",
+    link: "https://pt.wikipedia.org/wiki/Mateus_(evangelista)",
+    discipuloDe: "Nenhum mencionado explicitamente", // Provavelmente seguia a lei judaica antes de seguir Jesus
+    eventosImportantes: ["Chamado por Jesus", "Banquete em sua casa", "Autor do Evangelho de Mateus"],
+    patronoDe: ["Contadores, banqueiros, economistas - Devido à sua profissão anterior "], // Devido à sua profissão anterior
+    festaLiturgica: "21 de setembro",
+    iconografia: "Ángelus, livro, bolsa - O ângelus representa a anunciação, o livro seu evangelho e a bolsa sua profissão anterior", // O ângelus representa a anunciação, o livro seu evangelho e a bolsa sua profissão anterior
+    milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+    escritos: ["Evangelho de Mateus"]
   },
   {
-    nome: "filipe",
-    periodo: "Século I",
-    localNascimento: "Betsaiada, Galileia",
-    outrasInformacoes: "Evangelizou a região da Frígia e a Etiópia.",
-    link: "https://pt.wikipedia.org/wiki/Filipe_(ap%C3%B3stolo)"
+    nome: "Tome",
+    tags: "tome",
+    apelidos: ["Didimo, Significa gêmeo em aramaico"], // Significa "gêmeo" em aramaico
+    periodo: "c. 1º século d.C.",
+    localNascimento: "Desconhecido",
+    outrasInformacoes: "Um dos Doze Apóstolos, conhecido por sua dúvida sobre a ressurreição de Jesus, mas que, ao ver as chagas de Cristo, exclamou: 'Meu Senhor e meu Deus!'.",
+    link: "https://pt.wikipedia.org/wiki/Tom%C3%A9_(ap%C3%B3stolo)",
+    discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+    eventosImportantes: ["Dúvida sobre a ressurreição", "Confissão após ver as chagas de Jesus"],
+    patronoDe: ["Arquitetos, geômetras - Tradicionalmente associado a atividades que exigem precisão"], // Tradicionalmente associado a atividades que exigem precisão
+    festaLiturgica: "3 de outubro",
+    iconografia: "Lança, régua - A lança simboliza a dúvida e a régua a busca pela verdade", // A lança simboliza a dúvida e a régua a busca pela verdade
+    milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+    escritos: ["Não há escritos canônicos atribuídos a Tomé"] // Não há escritos canônicos atribuídos a Tomé
   },
   {
-    nome: "bartolomeu",
-    periodo: "Século I",
-    outrasInformacoes: "Identificado por alguns estudiosos como Natanael, evangelizou a Índia e a Etiópia.",
-    link: "https://pt.wikipedia.org/wiki/Bartolomeu_(ap%C3%B3stolo)"
+    nome: "Tiago, filho de Alfeu",
+    apelidos: ["Tiago, o Menor, Às vezes chamado assim para diferenciá-lo de Tiago, irmão de João"], // Às vezes chamado assim para diferenciá-lo de Tiago, irmão de João
+    periodo: "c. 1º século d.C.",
+    localNascimento: "Desconhecido",
+    outrasInformacoes: "Um dos Doze Apóstolos, menos conhecido que outros. Sua mãe, Maria, é mencionada junto com a de Jesus entre as mulheres que acompanharam Jesus durante seu ministério.",
+    link: "https://www.gty.org/library/sermons-library/POR-2275/os-homens-do-mestre-parte-4-tiago-filho-de-alfeu", // Um recurso para mais informações
+    discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+    eventosImportantes: ["Escolhido como um dos Doze Apóstolos"],
+    patronoDe: ["Não há registros específicos de grupos ou lugares sob sua proteção"], // Não há registros específicos de grupos ou lugares sob sua proteção
+    festaLiturgica: "9 de maio", // Data variável dependendo do calendário litúrgico
+    iconografia: "Serra - Simboliza seu martírio por ser serrado ao meio", // Simboliza seu martírio por ser serrado ao meio
+    milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+    escritos: [" Não há escritos canônicos atribuídos a ele"] // Não há escritos canônicos atribuídos a ele
   },
   {
-    nome: "mateus",
-    periodo: "Século I",
-    outrasInformacoes: "Publicano antes de ser chamado por Jesus, autor do Evangelho de Mateus.",
-    link: "https://pt.wikipedia.org/wiki/Mateus_(evangelista)"
-  },
-  {
-    nome: "tomé",
-    periodo: "Século I",
-    outrasInformacoes: "Conhecido por duvidar da ressurreição de Jesus, mas posteriormente creu.",
-    link: "https://pt.wikipedia.org/wiki/Tomé_(apóstolo)"
-  },
-  {
-    nome: "tiago, filho de Alfeu",
-    periodo: "Século I",
-    outrasInformacoes: "Irmão mais jovem de Judas Tadeu.",
-    link: "https://pt.wikipedia.org/wiki/Tiago_menor"
-  },
-  {
-    nome: "judas Tadeu (ou Judas Lebeu)",
-    periodo: "Século I",
-    outrasInformacoes: "Irmão de Tiago, filho de Alfeu.",
-    link: "https://pt.wikipedia.org/wiki/Judas_Tadeu"
-  },
-  {
-    nome: "simão, o Zelote",
-    periodo: "Século I",
-    outrasInformacoes: "Membro do partido zelote, que se opunha à ocupação romana.",
-    link: "https://pt.wikipedia.org/wiki/Sim%C3%A3o_o_Zelote"
-  },
-  {
-    nome: "judas Iscariotes",
-    periodo: "Século I",
-    outrasInformacoes: "Traiu Jesus por 30 moedas de prata.",
-    link: "https://pt.wikipedia.org/wiki/Judas_Iscariotes"
-  },
-  {
-    nome: "paulo de Tarso",
-    periodo: "Século I",
-    localNascimento: "Tarso, Cilícia (atual Turquia)",
-    localMartirio: "Roma, Itália",
-    martirio: "Decapitação",
-    perseguidor: "Imperador Nero",
-    outrasInformacoes: "Considerado um dos mais importantes teólogos do cristianismo. Escreveu diversas epístolas que fazem parte do Novo Testamento.",
-    link: "https://pt.wikipedia.org/wiki/Paulo_de_Tarso"
+    nome: "Judas Tadeu",
+    apelidos: ["Judas Lebbeu", "Judas o Galileu"],
+    periodo: "c. 1º século d.C.",
+    localNascimento: "Desconhecido",
+    outrasInformacoes: "Um dos Doze Apóstolos, frequentemente confundido com Judas Iscariotes. É considerado um dos apóstolos menos conhecidos, mas sua carta, incluída no Novo Testamento, revela um profundo conhecimento das Escrituras e um grande amor pelos irmãos em Cristo.",
+    link: "https://www.bibliaonline.com.br/dicionario/judas-tadeu/",
+    discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+    eventosImportantes: ["Escolhido como um dos Doze Apóstolos", "Autor da Epístola de Judas"],
+    patronoDe: ["Causas impossíveis, situações desesperadas - Considerado o santo dos casos desesperados"], // Considerado o santo dos casos desesperados
+    festaLiturgica: "28 de junho",
+    iconografia: "Clava, livro - A clava simboliza o martírio e o livro, sua autoria da epístola ", // A clava simboliza o martírio e o livro, sua autoria da epístola
+    milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+    escritos: ["Epístola de Judas"]
+  }
+,
+{
+  nome: "Simão, o Zelote",
+  periodo: "c. 1º século d.C.",
+  localNascimento: "Desconhecido",
+  outrasInformacoes: "Um dos Doze Apóstolos, anteriormente membro do partido zelota, um grupo revolucionário judeu que buscava a libertação de Roma. Seu nome indica sua origem e suas convicções políticas antes de seguir Jesus.",
+  link: "https://www.bibliaonline.com.br/dicionario/simao-o-zelota/",
+  discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+  eventosImportantes: ["Escolhido como um dos Doze Apóstolos"],
+  patronoDe: ["Políticos, revolucionários - Devido à sua origem zelota"], // Devido à sua origem zelota
+  festaLiturgica: ["Não há uma data específica celebrada universalmente"],
+  iconografia: "Espada - Simboliza sua origem zelota e a luta pela justiça", // Simboliza sua origem zelota e a luta pela justiça
+  milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+  escritos: ["Não há escritos canônicos atribuídos a ele"] // Não há escritos canônicos atribuídos a ele
+}
+,
+{
+  nome: "Judas Iscariotes",
+  apelidos: ["traidor"],
+  periodo: "c. 1º século d.C.",
+  localNascimento: "Desconhecido",
+  outrasInformacoes: "Um dos Doze Apóstolos, conhecido por ter traído Jesus por 30 moedas de prata. Seu ato é um dos eventos mais trágicos da narrativa bíblica.",
+  link: "https://pt.wikipedia.org/wiki/Judas_Iscariotes",
+  discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+  eventosImportantes: ["Traição de Jesus", "Suicídio"],
+  patronoDe: ["Geralmente não é considerado um santo e não tem patrocínios atribuídos"], // Geralmente não é considerado um santo e não tem patrocínios atribuídos
+  iconografia: "Saco de dinheiro, árvore da forca - Simbolizando a traição e o suicídio", // Simbolizando a traição e o suicídio
+  milagresAtribuidos: ["Não há registros de milagres, mas sim de um ato trágico"], // Não há registros de milagres, mas sim de um ato trágico
+  escritos: ["Não há escritos canônicos atribuídos a ele"] // Não há escritos canônicos atribuídos a ele
+}
+,
+{
+  nome: "Paulo",
+  apelidos: ["Saulo de Tarso"],
+  periodo: "c. 5-67 d.C.",
+  localNascimento: "Tarso, Cilícia",
+  localMartirio: "Roma",
+  martirio: "Decapitado",
+  perseguidor: "Nero",
+  outrasInformacoes: "Converso famoso, autor de várias epístolas, considerado o 'apóstolo dos gentios'.",
+  link: "https://pt.wikipedia.org/wiki/Paulo_de_Tarso",
+  discipuloDe: "Jesus (após a conversão)",
+  eventosImportantes: ["Conversão na estrada de Damasco", "Missões missionárias"],
+  patronoDe: ["Teólogos, missionários"],
+  festaLiturgica: "29 de junho",
+  iconografia: "Espada, livro",
+  milagresAtribuidos: ["Cura de Eusébio", "Ressurreição de Eurídice"],
+  escritos: ["Romanos", "Coríntios", "Gálatas", "Efésios", "Filipenses", "Colossenses", "Tessalonicenses", "Timóteo", "Tito", "Filemom", "Hebreus"]
 },
 {
-    nome: "matias",
-    periodo: "Século I",
-    localNascimento: "Desconhecido",
-    localMartirio: "Colchis, Ásia Menor (atual Geórgia)",
-    martirio: "Apedrejamento e decapitação",
-    perseguidor: "Desconhecido",
-    outrasInformacoes: "Escolhido para substituir Judas Iscariotes como apóstolo. Menos informações detalhadas sobre sua vida.",
-    link: "https://pt.wikipedia.org/wiki/Matias"
+  nome: "Matias",
+  periodo: "c. 1º século d.C.",
+  localNascimento: "Desconhecido",
+  outrasInformacoes: "Um dos Doze Apóstolos, escolhido por sorteio para substituir Judas Iscariotes após a traição. Pouco se sabe sobre sua vida antes ou depois de se tornar apóstolo.",
+  link: "https://www.bibliaonline.com.br/dicionario/matias/",
+  discipuloDe: "João Batista - Provavelmente seguia a lei judaica antes de seguir Jesus", // Provavelmente seguia a lei judaica antes de seguir Jesus
+  eventosImportantes: ["Escolhido para substituir Judas Iscariotes"],
+  patronoDe: ["Não há registros específicos de grupos ou lugares sob sua proteção"], // Não há registros específicos de grupos ou lugares sob sua proteção
+  festaLiturgica: "14 de maio", // Data variável dependendo do calendário litúrgico
+  iconografia: "Saco de dinheiro -  Simboliza a substituição de Judas Iscariotes", // Simboliza a substituição de Judas Iscariotes
+  milagresAtribuidos: ["Não há registros de milagres específicos atribuídos a ele"], // Não há registros de milagres específicos atribuídos a ele
+  escritos: [" Não há escritos canônicos atribuídos a ele"] // Não há escritos canônicos atribuídos a ele
 }
 ];
