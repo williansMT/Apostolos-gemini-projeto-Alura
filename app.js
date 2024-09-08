f6e3d6a8acb3d89e60201d09f0918df2dc40cc63
williansMT <65174371+williansMT@users.noreply.github.com>
2024-09-07 22:08:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function pesquisar() {
     // 3. Inicializa uma string vazia para armazenar os resultados da pesquisa.
     let resultados = "";
     let nome = "";
-    
+    let tags = "";
     // 4. Itera sobre cada elemento (dado) do array 'dados', que representa um apóstolo.
     for (let dado of dados) {
         nome = dado.nome.toLowerCase()
@@ -27,14 +27,25 @@ function pesquisar() {
             //    A sintaxe `${variável}` permite inserir o valor de uma variável diretamente no texto.
             resultados += `
                 <div class="item-resultado">
-        <h2> ${dado.nome ?? "Não informado"}</h2>
+        <h2> ${dado.nome ?? "Não tem ou Não informado"}</h2>
         <p>
-        Período: ${dado.periodo ?? "Não informado"}<br>
-        Local de nascimento: ${dado.localNascimento ?? "Não informado"}<br>
-        Local de martírio: ${dado.localMartirio ?? "Não informado"}<br>
-        Martírio: ${dado.martirio ?? "Não informado"}<br>
-        Perseguidor: ${dado.perseguidor ?? "Não informado"}<br>
-        Outras informações: ${dado.outrasInformacoes ?? "Não informado"}
+        
+        Apelidios: ${dado.apelidos ?? "Não tem ou Não informado"}<br>
+        Período: ${dado.periodo ?? "Não tem ou Não informado"}<br>
+        Local de trabalho: ${dado.localTrabalho ?? "Não tem ou Não informado"}<br>
+        Local de nascimento: ${dado.localNascimento ?? "Não tem ou Não informado"}<br>
+        Profissão: ${dado.profissao ?? "Não tem ou Não informado"}
+        Local de martírio: ${dado.localMartirio ?? "Não tem ou Não informado"}<br>
+        Martírio: ${dado.martirio ?? "Não tem ou Não informado"}<br>
+        Perseguidor: ${dado.perseguidor ?? "Não tem ou Não informado"}<br>
+        Outras informações: ${dado.outrasInformacoes ?? "Não tem ou Não informado"} <br>
+        Discipulo de: ${dado.discipuloDe ?? "Não tem ou Não informado"}<br>
+        Eventos Importantes: ${dado.eventosImportantes ?? "Não tem ou Não informado"} <br>
+        Patrono de: ${dado.patronoDe ?? "Não tem ou Não informado"} <br>
+        Festa Liturgica: ${dado.festaLiturgica ?? "Não tem ou Não informado"} <br>
+        iconografia: ${dado.iconografia ?? "Não tem ou Não informado"}<br>
+        Milagres Atribuidos: ${dado.milagresAtribuidos ?? "Não tem ou Não informado"}<br>
+        Escritos: ${dado.escritos ?? "Não tem ou Não informado"} <br>
         </p>
         <a href="${dado.link ?? "Link não disponível"}" target="_blank">wikipedia </a>
     </div>
